9b93d1960ebfc5f01d5aad7d33f04c11c9130d2d
sean stephen brian <seanstephenbrian@gmail.com>
2022-12-13 14:28:37 -0500

    write method to find square from x & y coords

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const board = () => {
 
+    // create a gameboard with 64 squares...
     let squares = [];
     for (x = 0; x <= 7; x++) {
         for (y = 0; y <= 7; y++) {
@@ -8,39 +9,53 @@ const board = () => {
         }
     }
 
+    // method to find a square from its x & y coordinates:
+    function findSquare(x, y) {
+        let targetSquare;
+        squares.forEach(square => {
+            if (square.x === x && square.y === y) {
+                targetSquare = square;
+            }
+        });
+        return targetSquare;
+    }
+
     // default knight position:
     let knight = [0, 0];
 
-    return { squares, knight }
+    return { squares, knight, findSquare }
 }
 
+// each square has an x coordinate value, a y coordinate value, 
+// and a 'paths' array which will contain a list of the squares to which the knight can travel:
 const square = (x, y) => {
     let paths = [];
 
-    if ((x - 1) >= 0 && (y + 2) <= 7) {
-        paths.push([x - 1, y + 2]);
-    }
-    if ((x + 1) <= 7 && (y + 2) <= 7) {
-        paths.push([x + 1, y + 2]);
-    }
-    if ((x - 2) >= 0 && (y + 1) <= 7) {
-        paths.push([x - 2], [y + 1]);
-    }
-    if ((x + 2) <= 7 && (y + 1) <= 7) {
-        paths.push([x + 2, y + 1]);
-    }
-    if ((x - 2) >= 0 && (y - 1) >= 0) {
-        paths.push([x - 2, y - 1]);
-    }
-    if ((x + 2) <= 7 && (y - 1) >= 0) {
-        paths.push([x + 2, y - 1]);
-    }
-    if ((x - 1) >= 0 && (y - 2) >= 0) {
-        paths.push([x - 1, y - 2]);
-    }
-    if ((x + 1) <= 7 && (y - 2) >= 0) {
-        paths.push([x + 1, y - 2]);    
-    }
+
+    // if ((x - 1) >= 0 && (y + 2) <= 7) {
+    //     paths.push([x - 1, y + 2]);
+    // }
+    // if ((x + 1) <= 7 && (y + 2) <= 7) {
+    //     paths.push([x + 1, y + 2]);
+    // }
+    // if ((x - 2) >= 0 && (y + 1) <= 7) {
+    //     paths.push([x - 2], [y + 1]);
+    // }
+    // if ((x + 2) <= 7 && (y + 1) <= 7) {
+    //     paths.push([x + 2, y + 1]);
+    // }
+    // if ((x - 2) >= 0 && (y - 1) >= 0) {
+    //     paths.push([x - 2, y - 1]);
+    // }
+    // if ((x + 2) <= 7 && (y - 1) >= 0) {
+    //     paths.push([x + 2, y - 1]);
+    // }
+    // if ((x - 1) >= 0 && (y - 2) >= 0) {
+    //     paths.push([x - 1, y - 2]);
+    // }
+    // if ((x + 1) <= 7 && (y - 2) >= 0) {
+    //     paths.push([x + 1, y - 2]);    
+    // }
 
     return { x, y, paths }
 }
@@ -52,3 +67,5 @@ function knightMoves(start, end) {
 
 const myBoard = board();
 console.log(myBoard.squares);
+const mySquare = myBoard.findSquare(4, 4);
+console.log(mySquare)
