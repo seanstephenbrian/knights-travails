e53048d25a74e7027612a82d92f3b015f370aa1c
sean stephen brian <seanstephenbrian@gmail.com>
2022-12-15 10:15:19 -0500

    create working solution!!
    added a new previousSquare property to each square to help record the
    knight's successful path; this only works because a visitedSquares set
    prevents the knight from visiting the same square twice...
    
    utility functions reconstruct the path and log it to the console, then
    clear the previousSquare values from all squares on the board
    
    i cleaned up the code and added comments throughout to explain the logic
    
    also tried to make the algorithm a little smarter by checking each
    potential move's potential moves... if a move's moves contain the ending
    square, it is added to the front of the queue, not the end of it

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const board = () => {
         return targetSquare;
     }
 
-    // add moves to each square node:
+    // add possible moves to each square node:
     squares.forEach(square => {
         if ((square.x - 1) >= 0 && (square.y + 2) <= 7) {
             square.moves.push(findSquare((square.x - 1), (square.y + 2)));
@@ -48,180 +48,96 @@ const board = () => {
         }
     });
 
-    // default knight position:
-    let knight = [0, 0];
 
     function knightMoves(start, end) {
+
+        // find the actual squares based on the input coordinates:
         const startingSquare = findSquare(start[0], start[1]);
         const endingSquare = findSquare(end[0], end[1]);
-        let path = [];
+
+        // create a set to track which squares have been visited 
+        // & ensure the knight doesn't go back to them:
         const visitedSquares = new Set();
 
+        // the main search loop...
+        // it uses a breadth-first approach to queue up squares that have not yet been visited:
         search: {
             let queue = [];
-            let previousSquare = null;
-            let currentSquare = null;
             queue.push(startingSquare);
             while (queue.length > 0) {
-                previousSquare = currentSquare;
-                currentSquare = queue.shift();
+                // the currentSquare is taken from the front of the queue:
+                let currentSquare = queue.shift();
+                // the currentSquare is added to the visitedSquares set to ensure it doesn't get visited again:
                 visitedSquares.add(currentSquare);
-                path.push([previousSquare, currentSquare]);
+                // check if the currentSquare's possible moves include the target endingSquare;
+                // if they do, set the endingSquare's previousSquare property to the currentSquare, then shut down the search:
                 if (currentSquare.moves.includes(endingSquare)) {
-                    path.push([currentSquare, endingSquare]);
+                    endingSquare.previousSquare = currentSquare;
                     break search;
                 } else {
+                    // otherwise, iterate through all of the moves available from the currentSquare...
                     currentSquare.moves.forEach(move => {
-                        if (move.moves.includes(endingSquare)) {
-                            queue.unshift(move);
-                        }
-                        move.moves.forEach(move => {
+                        // only queue up squares that have not yet been visited:
+                        if (!visitedSquares.has(move)) {
+                            // set the next square's previousSquare property to the currentSquare:
+                            move.previousSquare = currentSquare;
+                            // logic to prioritize moves which have a move that leads directly to endingSquare:
                             if (move.moves.includes(endingSquare)) {
                                 queue.unshift(move);
+                            } else {
+                                queue.push(move);
                             }
-                        });
-                        
-                        if (!visitedSquares.has(move)) {
-                            queue.push(move);
                         }
                     });
                 }
             }
         }
 
-        console.log(visitedSquares)
-        console.log(path);
+        recordPath(startingSquare, endingSquare);
+
+        clearPreviousSquares();
 
     }
 
+    // method to record the successful path using the visited squares' previousSquare values:
+    function recordPath(startingSquare, endingSquare) {
+        let path = [];
+        let currentSquare = endingSquare;
+        while (currentSquare !== startingSquare) {
+            path.unshift(currentSquare);
+            currentSquare = currentSquare.previousSquare;
+        }
+        path.unshift(startingSquare);
+        if (path.length === 2) {
+            console.log(`You made it from [${startingSquare.x}, ${startingSquare.y}] to [${endingSquare.x}, ${endingSquare.y}] in ${path.length - 1} move!`);
+        } else {
+            console.log(`You made it from [${startingSquare.x}, ${startingSquare.y}] to [${endingSquare.x}, ${endingSquare.y}] in ${path.length - 1} moves!`);
+        }
+        console.log('Your path was:');
+        path.forEach(move => {
+            console.log(move.x + ', ' + move.y);
+        })
+    }
 
+    // clears out the previousSquare values from all the squares on the board:
+    function clearPreviousSquares() {
+        squares.forEach(square => {
+            square.previousSquare = null;
+        })
+    }
 
-
-
-
-
-
-
-
-
-    // function knightMoves(start, end) {
-    //     let startingSquare = findSquare(start[0], start[1]);
-    //     let endingSquare = findSquare(end[0], end[1]);
-
-    //     let pathIdentified = false;
-    //     let finalPath = [];
-
-    //     function knightMovesRec(start, path = [start]) {
-    //         if (start === endingSquare) {
-    //             finalPath = path;
-    //             pathIdentified = true;
-    //         } else if (start !== endingSquare) {
-    //             start.moves.forEach(move => {
-    //                 path.push(move);
-    //                 knightMovesRec(move, path);
-    //             });
-    //         }
-    //     }
-
-    //     do {
-    //         knightMovesRec(startingSquare);
-    //     } while (pathIdentified === false);
-
-    //     const moves = finalPath.length - 1;
-
-    //     if (moves === 1) {
-    //         console.log(`You made it in ${moves} move. Your path was:`);
-    //     } else {
-    //         console.log(`You made it in ${moves} moves. Your path was:`);
-    //     }
-    //     moves.forEach(move => {
-    //         console.log(move.x + ', ' + move.y);
-    //     });
-    // }
-
-    // function knightMoves(start, end) {
-
-    //     let pathIdentified = false;
-    //     const endSquare = findSquare(end[0], end[1]);  
-    //     let counter = 0;
-
-    //     while (counter < 7) {
-    //         function knightMovesRec(start, path = [findSquare(start[0], start[1])]) {
-
-    //             if (pathIdentified === true) return;
-    
-    //             const startSquare = findSquare(start[0], start[1]);
-                
-    //             startSquare.moves.forEach(move => {
-    //                 path.push(move);
-    //                 if (move === endSquare) {
-    //                     pathIdentified = true;
-    //                     if (counter === 1) {
-    //                         console.log(`You made it in ${counter} move. Your path was:`);
-    //                     } else {
-    //                         console.log(`You made it in ${counter} moves. Your path was:`);
-    //                     }
-    //                     path.forEach(move => {
-    //                         console.log(move.x + ', ' + move.y);
-    //                     });
-    //                     return;
-    //                 } else {
-    //                     counter++;
-    //                     knightMovesRec([move.x, move.y], path)
-    //                 }
-    //             });
-    //         }
-    //         knightMovesRec(start, end);
-            
-    //     }
-
-        
-
-    // }
-
-    // function knightMoves(start, end) {
-    //     let squareFound = false;
-
-    //     function knightMovesRecursion(start, end, counter = 1, path = [findSquare(start[0], start[1])]) {
-
-    //         const startSquare = findSquare(start[0], start[1]);
-    //         const endSquare = findSquare(end[0], end[1]);
-    //         startSquare.moves.forEach(move => {
-    //             if (move === endSquare) {
-    //                 squareFound = true;
-    //                 path.push(endSquare);
-    //                 if (counter === 1) {
-    //                     console.log(`You made it in ${counter} move. Your path was:`);
-    //                 } else {
-    //                     console.log(`You made it in ${counter} moves. Your path was:`);
-    //                 }
-    //                 path.forEach(move => {
-    //                     console.log(move.x + ', ' + move.y);
-    //                 });
-    //                 return;
-    //             } else if (counter < 5 && squareFound === false) {
-    //                 counter = counter + 1;
-    //                 path.push(move);
-    //                 knightMovesRecursion([move.x, move.y], [endSquare.x, endSquare.y], counter, path);
-    //             }
-    //         });
-    //     };
-
-    // //     knightMovesRecursion(start, end);
-    
-    // }
-
-    return { squares, knight, findSquare, knightMoves }
+    return { squares, findSquare, knightMoves }
 }
 
 // each square has an x coordinate value, a y coordinate value, 
-// and a 'moves' array which will contain a list of the squares to which the knight can travel:
+// a 'moves' array which will contain links to the squares to which the knight can travel,
+// and a previousSquare property to record the knight's eventual path:
 const square = (x, y) => {
     let moves = [];
-
-    return { x, y, moves }
+    let previousSquare = null;
+    return { x, y, moves, previousSquare }
 }
 
 const myBoard = board();
 
-myBoard.knightMoves([0,0], [7, 7]);
+myBoard.knightMoves([0,0], [2, 4]);
